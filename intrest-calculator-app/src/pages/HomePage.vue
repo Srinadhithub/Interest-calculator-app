9930ca5d1c997e7414bf031381cdec93bee24221
Srinadhithub <srinadhpodhili@gmail.com>
2024-12-07 23:45:18 +0530

    first commit

---
+++ intrest-calculator-app/src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>
+    <VCard elevation="2">
+      <VCardItem>
+        <VCardTitle>
+          In
+        </VCardTitle>
+      </VCardItem>
+      <VCardText>
+        <VBtn color="error">
+          Success
+        </VBtn>
+      </VCardText>
+    </VCard>
+  </div>
+</template>
